157fef494cf2d68f988550df01300071ccd5a71e
jinho-park <lucky112233335@gmail.com>
2023-11-24 16:03:10 +0900

    Update 1.build.sh

--- script/1.build.sh
+++ script/1.build.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
 controller_name="hybrid.analysis-engine"
 docker_id="ketidevit2"
 export GO111MODULE=on
 go mod vendor
+go build -o /root/workspace/pjh/dev/analysis-engine/build/_output/bin/$controller_name -mod=vendor /root/workspace/pjh/dev/analysis-engine/cmd/main.go
+=======
 go build -o /root/workspace/hth/dev/analysis-engine/build/_output/bin/$controller_name -mod=vendor /root/workspace/hth/dev/analysis-engine/cmd/main.go
 docker build -t $docker_id/$controller_name:latest /root/workspace/hth/dev/analysis-engine/build && \
 docker push $docker_id/$controller_name:latest
-
-
-#0.0.2 => 운용중인 metric collector member
-#0.0.3 => test 용 버전 (제병)
