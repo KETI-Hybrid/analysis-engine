6308db2c8859e38688e9ca711e4e42d0b7880459
jinho-park <lucky112233335@gmail.com>
2023-11-24 16:00:44 +0900

    Update 1.make_docker.sh

--- 1.make_docker.sh
+++ 1.make_docker.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+docker_id="ketidevit2"
+controller_name="hybrid.analysis-engine"
+docker build -t $docker_id/$controller_name:latest /root/workspace/pjh/dev/analysis-engine/build && \
+docker push $docker_id/$controller_name:latest
